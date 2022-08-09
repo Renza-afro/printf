53125a7ac3667d8554ae35b25e9c2c54a3199f45
kris <christopherugwu3@gmail.com>
2022-08-09 16:01:28 -0700

    printf

--- printf.c
+++ printf.c
@@ -1,20 +1,16 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdint.h>
-
-
 /**
 * _printf - function to print output to the console
 * @format: pointer to the list of arguemant passed to the function
 *
 * Return: bytes of element printed out, -1 on error
 */
-
 int _printf(const char *format, ...)
 {
 int returnValue, val;
 va_list args;
-
 va_start(args, format);
 if (format == NULL)
 return (-1);
@@ -22,26 +18,21 @@ if (format[0] == '%' && format[1] == '\0')
 return (-1);
 val = _vprintf(format, args);
 va_end(args);
-
 (returnValue) = val;
 return (returnValue);
 }
-
 /**
-* _vprintf - helper function for printf, takes the arguement passed to printf,
-*				and does the actual printing
+* _vprintf - helper function for printf, takes the arguement passed to printf,ad does the actual printing
 * @format: pointer to the list of arguement passed to printf to be printd
 * @args: place holder for the arguement passed of type va_list
 *
 * Return: bytes of element printed, -1 if no parameter is supplied
 */
-
 int _vprintf(const char *format, va_list args)
 {
 int state = 0, flag[6] = {0, 0, 0, 0, 0, 0}, is_long = 0, is_short = 0;
 int count = 0, print_count = 0, identifier_printed, reset = 1;
 int *resetPtr = &reset, *ptr = &is_long, *shortPtr = &is_short, tmp_count;
-
 while (format[count])
 {
 if (state == 0)
